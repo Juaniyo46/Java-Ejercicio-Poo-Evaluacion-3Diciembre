01cf436d5d9d942fbe879784f01a02c9ed2b8eec
Notebook-Juan <juan.alvarez@escuelaestech.es>
2018-12-03 21:48:35 +0100

    Ejercicio corregido a falta de comentarios.

---
+++ src/es/JuanAntonio/Lista.java
@@ -0,0 +1,13 @@
+package es.JuanAntonio;
+
+import java.util.ArrayList;
+
+public class Lista {
+
+    public ArrayList<Casas> listas;
+
+
+    public Lista() {
+        listas = new ArrayList<>();
+    }
+}
--- src/es/JuanAntonio/Main.java
+++ src/es/JuanAntonio/Main.java
@@ -2,26 +2,48 @@ package es.JuanAntonio;
 
 public class Main {
 
+    static Lista casa;
+
     public static void main(String[] args) {
 
-        Casas casa1 = new Casas("Calle Lonja 3, Linares, Jaén", 23700,3,90);
-        Casas casa2 = new Casas("Calle Bailen 6, Mengibar, Jaén", 23620, 5, 190);
-        Casas casa3 = new Casas("Calle Los Grillares 8, Mengibar, Jaén", 23620,2,200);
+       casa = new Lista();
+       casa.listas.add(new Casas("Calle Lonja 3, Linares, Jaén", 23700,3,80));
+       casa.listas.add(new Casas("Calle  Bailen 5, Mengíbar,Jaén",23620,6,95));
+       casa.listas.add(new Casas("Calle Los Grillares 8, Mengíbar, Jaén", 23620,2,200));
 
-        String listAddress = casa1.getAddress()+"\n" + casa2.getAddress()+"\n" + casa3.getAddress();
 
-        String listPostalCode = casa1.getPostalCode() + "\n" + casa2.getPostalCode() + "\n" + casa3.getPostalCode();
+            int i = 0;
+        System.out.println("Lista de Direcciones");
+            for (Casas casa: casa.listas) {
+                System.out.println(casa.address);
+                i++;
+            }
+        System.out.println("Lista de Codigos Postal");
+            for (Casas casa: casa.listas) {
+                System.out.println(casa.postalCode);
+                 i++;
+            }
+        System.out.println("Lista de Nº de habitantes");
+            for (Casas casa: casa.listas) {
+                System.out.println(casa.population);
+                 i++;
+            }
+        System.out.println("Lista de Metros cuadrados");
+            for (Casas casa: casa.listas) {
+                System.out.println(casa.squareMeter);
+                i++;
+            }
 
-        String listPopulation = casa1.getPopulation() + "\n" + casa2.getPopulation() + "\n" + casa3.getPopulation();
 
-        String listSquareMeter = casa1.getSquareMeter() + "\n" + casa2.getSquareMeter() + "\n" + casa3.getSquareMeter();
-
-        System.out.println(listAddress);
-        System.out.println(listPostalCode);
-        System.out.println(listPopulation);
-        System.out.println(listSquareMeter);
 
     }
 
 
-}
+
+
+
+
+    }
+
+
+
